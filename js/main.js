21f7f6de4802826ab1e534af2a6cc27f81f99237
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-22 17:06:08 +0200

    Funzione navigazione pagine (total_pages)

--- js/main.js
+++ js/main.js
@@ -16,9 +16,8 @@ $(document).ready(function(){
   $(".search_input").keyup(function(){
 
     if(event.which == 13){
-      $("#movies-list").html("");
-
       var searchMovie = $(".search_input").val();
+      $(".search_bar").attr("data-search", searchMovie);
       stampaFilm(searchMovie);
 
       $(".search_input").val("");
@@ -26,10 +25,26 @@ $(document).ready(function(){
 
   });
 
+  //click sul quadratino prende il valore di data-page e passiamo il valore alla funzione stampaFilm
+  $(document).on("click", ".numero_pagine", function(){
+    //seleziono con il this
+    var numeroPagina = $(this).attr("data-page");
+    var lastSearch = $(".search_bar").attr("data-search");
+    stampaFilm(lastSearch, numeroPagina);
+  });
+
 });
 
 //Printa il risultato della risposta a schermo
-function stampaFilm(cercaFilm){
+function stampaFilm(cercaFilm, pagina){
+  //svuoto contenuti
+  $("#movies-list").html("");
+  $(".movies-page-list").html("");
+
+  //pagina==null
+  if(pagina == ""){
+    var pagina = 1;
+  }
   var api_key = "e985f53e1e87b07c7fd1095468f025a0";
 
   //preparo il template
@@ -42,13 +57,14 @@ function stampaFilm(cercaFilm){
       "data": {
         "api_key" : api_key,
         "language": "it-IT",
-        "page": 1,
+        "page": pagina,
         "query": cercaFilm
       },
       "method": "GET",
       "success": function (data, stato) {
         //array del risultato
         var results = data.results;
+        var pageNumber = data.total_pages;
 
         for(var i = 0; i < results.length; i++){
 
@@ -60,13 +76,30 @@ function stampaFilm(cercaFilm){
           };
 
           var html = template(context);
-
           $("#movies-list").append(html);
         }
+        stampaPagine(pageNumber);
       },
       "error": function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errori);
       }
     }
   );
+}
+
+//stampo il numero di pagine della chiamata
+function stampaPagine(numeroPagine){
+  //preparo il template
+  var source = $("#pages-template").html();
+  var template = Handlebars.compile(source);
+
+  for(var i = 1; i <= numeroPagine; i++){
+    var context = {
+      "page": i
+    }
+
+    var html = template(context);
+
+    $(".movies-page-list").append(html);
+  }
 }
