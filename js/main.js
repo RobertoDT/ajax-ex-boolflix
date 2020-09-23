6ae1e8c048eb3cf1fac0602c96b6ad42e952165f
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-23 12:05:42 +0200

    Refactoring Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 $(document).ready(function(){
+  //variabile globale
+  var lastSearch = "";
+
 
   // funzione cerca al click sul bottone
   $(".search_button").click(function(){
-    //svuoto subito il contenuto della lista del momento
-    $("#movies-list").html("");
     //prendo il valore dell'input e lo salvo in variabile
     var searchMovie = $(".search_input").val();
-    //invoco funzione per stampare a schermo la lista di film
-    stampaFilm(searchMovie);
+    lastSearch = $(".search_input").val();
+    if(searchMovie != ""){
+      //invoco funzione per stampare a schermo la lista di film
+      clear();
+      getMovies(searchMovie);
 
-    //svuoto il valore della input del momento
-    $(".search_input").val("");
+      //svuoto il valore della input del momento
+      $(".search_input").val("");
+    }
   });
 
   // funzione cerca premendo invio
@@ -19,10 +24,13 @@ $(document).ready(function(){
     if(event.which == 13){
       var searchMovie = $(".search_input").val();
       //setto l'attriburo come valore della input per salvarlo
-      $(".search_bar").attr("data-search", searchMovie);
-      stampaFilm(searchMovie);
+      lastSearch = $(".search_input").val();
+      if(searchMovie != ""){
+        clear();
+        getMovies(searchMovie);
 
-      $(".search_input").val("");
+        $(".search_input").val("");
+      }
     }
 
   });
@@ -31,27 +39,16 @@ $(document).ready(function(){
   $(document).on("click", ".numero_pagine", function(){
     //seleziono con il this cioè quel quadratino che vado a selezionare
     var numeroPagina = $(this).attr("data-page");
-    var lastSearch = $(".search_bar").attr("data-search");
-    stampaFilm(lastSearch, numeroPagina);
+    getMovies(lastSearch, numeroPagina);
+    clear();
   });
 
 });
+// /document
 
-//Printa il risultato della risposta a schermo con i film e il numero di pagine
-function stampaFilm(cercaFilm, pagina){
-  //svuoto contenuti della lista di film e dei quadratini
-  $("#movies-list").html("");
-  $(".movies-page-list").html("");
-
-  //pagina==null
-  // if(pagina == ""){
-  //   var pagina = 1;
-  // }
+//FUNZIONI
+function getMovies(cercaFilm, pagina){
   var api_key = "e985f53e1e87b07c7fd1095468f025a0";
-
-  //preparo il template
-  var source = $("#movies-template").html();
-  var template = Handlebars.compile(source);
 
   $.ajax(
     {
@@ -64,24 +61,10 @@ function stampaFilm(cercaFilm, pagina){
       },
       "method": "GET",
       "success": function (data, stato) {
-        //array del risultato
-        var results = data.results;
-        var pageNumber = data.total_pages;
 
-        for(var i = 0; i < results.length; i++){
+        stampaFilm(data);
+        stampaPagine(data);
 
-          var context = {
-            "title": results[i].title,
-            "original_title": results[i].original_title,
-            "original_language" : results[i].original_language,
-            "vote_average": results[i].vote_average
-          };
-
-          var html = template(context);
-          $("#movies-list").append(html);
-        }
-        //chiamo funzione per stampare i quadratini con il numero di pagine
-        stampaPagine(pageNumber);
       },
       "error": function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errori);
@@ -90,14 +73,46 @@ function stampaFilm(cercaFilm, pagina){
   );
 }
 
+
+//Printa il risultato della risposta a schermo con i film e il numero di pagine
+function stampaFilm(data, pagina){
+
+  //pagina==null
+  // if(pagina == ""){
+  //   var pagina = 1;
+  // }
+
+  //preparo il template
+  var source = $("#movies-template").html();
+  var template = Handlebars.compile(source);
+
+  //array del risultato
+  var results = data.results;
+
+  for(var i = 0; i < results.length; i++){
+
+    var context = {
+      "title": results[i].title,
+      "original_title": results[i].original_title,
+      "original_language" : results[i].original_language,
+      "vote_average": results[i].vote_average
+    };
+
+    var html = template(context);
+    $("#movies-list").append(html);
+  }
+}
+
+
 //stampo il numero di pagine della chiamata
-function stampaPagine(numeroPagine){
+function stampaPagine(data){
+  var pageNumber = data.total_pages;
   //preparo il template
   var source = $("#pages-template").html();
   var template = Handlebars.compile(source);
 
   //ciclo i numeri delle pagine prodotte dal risutato della query e compilo il context
-  for(var i = 1; i <= numeroPagine; i++){
+  for(var i = 1; i <= pageNumber; i++){
     var context = {
       "page": i
     }
@@ -106,4 +121,11 @@ function stampaPagine(numeroPagine){
     //stampo nel DOM
     $(".movies-page-list").append(html);
   }
+}
+
+
+function clear(){
+  //svuoto contenuti della lista di film e dei quadratini
+  $("#movies-list").html("");
+  $(".movies-page-list").html("");
 }
